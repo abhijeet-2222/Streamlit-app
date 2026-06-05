4185ab68cd0d8b8e73145ceedeb185f0efc2cab6
Abhijeet Pradhan <95130160+abhijeet-2222@users.noreply.github.com>
2026-06-05 12:04:02 +0530

    Add files via upload

---
+++ paa.py
@@ -0,0 +1,18 @@
+%%writefile app.py
+import streamlit as st
+import pandas as pd
+
+st.title("File Upload Demo 📂")
+
+# File uploader
+uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
+
+# Check if file is uploaded
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    
+    st.write("Here is your uploaded data:")
+    st.dataframe(df)
+else:
+    st.write("Please upload a file to view it.")
+
